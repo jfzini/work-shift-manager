bd55216ac839a7520fe0cf26bc7ee0d0768dcca0
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-26 20:59:10 -0300

    validating fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, redirect } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { createUser } from './services/userAPI';
 import Login from './pages/Login';
 
@@ -16,17 +16,15 @@ function App() {
 
   return (
       <Routes>
-        <Route path="/">
-          {logado
-            ? redirect("/search")
-            : <Login isLoading={isLoading} handleSubmit={handleSubmit} />}
-        </Route>
-        <Route path="/search" element={<Search />} />
+        <Route path="/" element={logado
+            ? <Navigate to="/search" />
+            : <Login isLoading={isLoading} handleSubmit={handleSubmit} />} />
+        {/* <Route path="/search" element={<Search />} />
         <Route path="/album/:id" element={<Album />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/profile/edit" element={<EditProfile />} />
-        <Route path="/*" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} /> */}
       </Routes>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,37 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from '../components/Loading';
-import './css/Login.css';
-import logo from '../images/sound-waves.png';
+// import './css/Login.css';
 
 function Login({ handleSubmit, isLoading }) {
   const [nameValue, setNameValue] = useState('');
-  const [validName, setValidName] = useState(false);
+  const [hoursValue, setHoursValue] = useState(8);
+  const [minutesValue, setMinutesValue] = useState(0);
+  const [validFields, setValidFields] = useState(false);
 
-  const validateName = (value) => {
+  const validateFields = () => {
     const minLength = 3;
-    setValidName(value.length >= minLength);
+    const validName = nameValue.length >= minLength;
+    const validHour = hoursValue <= 12 && hoursValue >=0
+    const validMinute = minutesValue <= 59 && minutesValue >=0
+    setValidFields(validName && validHour && validMinute);
   };
 
-  const handleChange = ({ target }) => {
-    const { value } = target;
-    setNameValue(value);
-    validateName(value);
+  useEffect(() => {
+    validateFields();
+  }, [nameValue, hoursValue, minutesValue])
+  
+  const handleChange = ({ target }, setStateFunc) => {
+    let { value } = target;
+    if (setStateFunc == setHoursValue) {
+      value = value > 12 ? 12 : value;
+      value = value < 0 ? 0 : value;
+    }
+    if (setStateFunc == setMinutesValue) {
+      value = value > 59 ? 59 : value;
+      value = value < 0 ? 0 : value;
+    }
+    setStateFunc(value);
   };
 
   return (
@@ -26,20 +41,40 @@ function Login({ handleSubmit, isLoading }) {
         : (
           <>
             <h2 className="login__title">Login</h2>
-            <img src={ logo } alt="logo" className="login__logo" />
+            {/* <img src={ logo } alt="logo" className="login__logo" /> */}
             <form action="" className="login__form">
               <label htmlFor="user-name">Nome:</label>
               <input
                 type="text"
                 id="user-name"
-                data-testid="login-name-input"
                 value={ nameValue }
-                onChange={ handleChange }
+                onChange={(e) => handleChange(e, setNameValue) }
               />
+              <fieldset>
+                <legend>Jornada</legend>
+              <label htmlFor="login-hours-shift">Horas:</label>
+              <input
+                type="number"
+                id="login-hours-shift"
+                value={ hoursValue }
+                placeholder='horas'
+                max="12"
+                onChange={(e) => handleChange(e, setHoursValue) }
+              />
+              <label htmlFor="login-minutes-shift">Minutos:</label>
+              <input
+                type="number"
+                id="login-minutes-shift"
+                value={ minutesValue }
+                placeholder='minutos'
+                max="59"
+                onChange={(e) => handleChange(e, setMinutesValue) }
+              />
+              </fieldset>
               <button
                 type="submit"
                 data-testid="login-submit-button"
-                disabled={ !validName }
+                disabled={ !validFields }
                 onClick={ () => handleSubmit(nameValue) }
               >
                 Entrar
