ff68f0e2977b017aebf8fee6d8869e0689451def
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-26 21:10:11 -0300

    setting route to userpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { createUser } from './services/userAPI';
 import Login from './pages/Login';
+import UserPage from './pages/UserPage';
 
 function App() {
   const [logado, setLogado] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [userName, setUserName] = useState('');
 
   const handleSubmit = async (name, hour, minutes) => {
     setIsLoading(true);
+    setUserName(name);
     await createUser({ name, hour, minutes });
     setLogado(true);
   };
@@ -17,8 +20,9 @@ function App() {
   return (
       <Routes>
         <Route path="/" element={logado
-            ? <Navigate to="/search" />
+            ? <Navigate to={`/user-page/${userName}`} />
             : <Login isLoading={isLoading} handleSubmit={handleSubmit} />} />
+        <Route path="/user-page/:name" element={<UserPage />} />
         {/* <Route path="/search" element={<Search />} />
         <Route path="/album/:id" element={<Album />} />
         <Route path="/favorites" element={<Favorites />} />
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CurrentShift from '../components/CurrentShift'
+
+export default function UserPage() {
+  return (
+    <CurrentShift />
+  )
+}
