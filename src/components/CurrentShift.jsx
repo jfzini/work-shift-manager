0a6fdfeae6582346284b715d0fa893551d6fa08d
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-07 20:27:42 -0300

    wip: calcShift + helpers

--- src/components/CurrentShift.jsx
+++ src/components/CurrentShift.jsx
@@ -2,71 +2,38 @@ import React, { useState } from 'react';
 import Fieldset from './TimePickerShift';
 import { DateField, DatePicker, TimePicker } from '@mui/x-date-pickers';
 import { DateTime } from 'luxon';
-import { saveShiftData } from '../services/storageHelpers';
+import { getShiftDuration, saveShiftData } from '../services/storageHelpers';
 import { CURRENT_SHIFT } from './data/currentShiftData';
 
 export default function CurrentShift() {
   const [value, setValue] = useState({ SHIFT_START: DateTime.local() });
-
+  //USAR A DATA NO FORMATO DD/MM/YYYY COMO CHAVE PARA O LOCAL STORAGE 
   const saveShift = (e) => {
     e.preventDefault();
     saveShiftData(value);
-  };
-
-  const getCurDate = () => {
-    const curMonth =
-      value.SHIFT_START.c.month >= 10
-        ? value.SHIFT_START.c.month
-        : `0${value.SHIFT_START.c.month}`;
-    const curDay =
-      value.SHIFT_START.c.day >= 10
-        ? value.SHIFT_START.c.day
-        : `0${value.SHIFT_START.c.day}`;
-    return `${value.SHIFT_START.c.year}-${curMonth}-${curDay}`;
+    calcShift();
   };
 
   const calcShift = () => {
     const prevData = localStorage.getItem('shiftData');
     if (prevData) {
       const parsedPrevData = JSON.parse(prevData);
-      const teste = parsedPrevData.map((date) => {
-        const shiftStart = new Date(date.SHIFT_START);
-        const breakStart = new Date(date.BREAK_START);
-        const breakEnd = new Date(date.BREAK_END);
-        const shiftEnd = new Date(date.SHIFT_END);
-
-        const shiftDuration = (shiftEnd - shiftStart) / (1000 * 60 * 60);
-        const breakDuration = (breakEnd - breakStart) / (1000 * 60 * 60);
-
-        const hoursWorked = shiftDuration - breakDuration;
-
-        const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
-        const testString = shiftStart.toLocaleDateString('pt-br', options);
-        return {[testString]: hoursWorked.toFixed(2)}
-      });
-      console.log(teste);
+      const shiftHistory = parsedPrevData.map((date) => getShiftDuration(date));
     }
   };
 
-  calcShift()
-
-  // const getShift = () => {
-  //   const prevData = localStorage.getItem('shiftData');
-  //   if (prevData) {
-  //     const parsedPrevData = JSON.parse(prevData);
-  //     const savedDates = parsedPrevData.map(({ SHIFT_START }) => SHIFT_START);
-  //     const curDate = getCurDate()
-  //     const testbool = savedDates.find((date) => date.includes(curDate))
-  //   }
-  // };
-
-  // getShift();
-
   return (
     <form>
+      {/*The date inserted below will overwrite the current date if the user wants to save a past date*/}
       <DatePicker
         value={value.SHIFT_START}
-        onChange={(newValue) => setValue({ ...value, SHIFT_START: newValue })}
+        onChange={(newValue) => setValue({
+          ...value,
+          SHIFT_START: newValue,
+          BREAK_START: newValue,
+          BREAK_END: newValue,
+          SHIFT_END: newValue,
+        })}
       />
       {CURRENT_SHIFT.map(({ label, id }) => (
         <TimePicker
--- src/services/storageHelpers.js
+++ src/services/storageHelpers.js
@@ -1,6 +1,22 @@
 export const saveShiftData = (shiftData) => {
   const prevData = localStorage.getItem('shiftData');
-  const parsedPrevData = prevData ? JSON.parse(prevData) : []
+  const parsedPrevData = prevData ? JSON.parse(prevData) : [];
   const shiftDataStringified = JSON.stringify([...parsedPrevData, shiftData]);
   localStorage.setItem('shiftData', shiftDataStringified);
-}
+};
+
+export const getShiftDuration = (shiftData) => {
+  const shiftStart = new Date(shiftData.SHIFT_START);
+  const breakStart = new Date(shiftData.BREAK_START);
+  const breakEnd = new Date(shiftData.BREAK_END);
+  const shiftEnd = new Date(shiftData.SHIFT_END);
+  
+  const shiftDuration = (shiftEnd - shiftStart) / (1000 * 60 * 60);
+  const breakDuration = (breakEnd - breakStart) / (1000 * 60 * 60);
+  
+  const hoursWorked = shiftDuration - breakDuration;
+  
+  const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+  const testString = shiftStart.toLocaleDateString('pt-br', options);
+  return { [testString]: hoursWorked.toFixed(2) };
+};
